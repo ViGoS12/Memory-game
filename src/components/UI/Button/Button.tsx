9187c788a26c81459f1d306f99c5fb51ecfe7b29
Kirill <kirisakov4@yandex.ru>
2022-08-25 19:17:26 +0300

    Added button component

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,23 @@
+import styles from './Button.module.scss'
+import { ReactNode } from 'react'
+
+interface IButtonProps {
+  children: ReactNode
+  onClick?: React.MouseEventHandler<HTMLElement>
+  color?: string
+}
+
+const Button: React.FC<IButtonProps> = ({ children, ...props }) => {
+  console.log(props.color)
+  return (
+    <button
+      {...props}
+      onClick={props.onClick}
+      className={styles.button}
+      style={{ backgroundColor: props.color || '#1cdfc8' }}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
